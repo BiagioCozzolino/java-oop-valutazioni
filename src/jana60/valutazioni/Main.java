329f0d33c485cf011b59920dc6bc49c987bb07a9
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-16 15:35:16 +0200

    Creazione attributi, costruttore e metodo, inizio Main

--- src/jana60/valutazioni/Main.java
+++ src/jana60/valutazioni/Main.java
@@ -1,10 +1,35 @@
 package jana60.valutazioni;
 
+import java.util.Scanner;
+
+import jana60.shop.Product;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		System.out.println("Ciao, benvenuto nel nuovo programma di valutazioni!");
+		
+		System.out.print("Inserisci quanti Alunni vuoi inserire: ");
+		int sceltautente = scan.nextInt();
+	
+		Studente[] listastudenti = new Studente[sceltautente];
 
+		
+		for (int i = 0; i < listastudenti.length; i++) {
+			System.out.println("Inserisci marca: ");
+			String marca = scan.nextLine();
+			System.out.println("Inserisci nome: ");
+			String nome = scan.nextLine();
+			System.out.println("Inserisci prezzo: ");
+			// Aggiunto il parse per evitare errori con lo scan int-double ecc.
+			float prezzo = Float.parseFloat(scan.nextLine());
+			System.out.println("Inserisci Iva: ");
+			int iva = Integer.parseInt(scan.nextLine());
 	}
 
 }
--- src/jana60/valutazioni/Studente.java
+++ src/jana60/valutazioni/Studente.java
@@ -2,4 +2,27 @@ package jana60.valutazioni;
 
 public class Studente {
 
+	// Attributi
+	int id;
+	int percAssenze;
+	double medVoti;
+
+	// Costruttore
+	Studente(int id, int percAssenze, double medVoti) {
+		super();
+		this.id = id;
+		this.percAssenze = percAssenze;
+		this.medVoti = medVoti;
+
+		if (percAssenze > 25 && percAssenze < 50 && medVoti > 2) {
+			System.out.println("L'alunno selezionato risulta Promosso");
+		} else if (percAssenze < 25 && medVoti >= 2) {
+			System.out.println("L'alunno selezionato risulta Promosso");
+		} else {
+			System.out.println("L'alunno selezionato risulta Bocciato");
+		}
+
+		// Metodi
+
+	}
 }
