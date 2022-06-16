c0ea83825f2f47d3f1ff88749c0019f70fb04a4d
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-16 16:52:24 +0200

    Terminato sezione main con lista studenti e stampa bocciato promosso con numero progressivo

--- src/jana60/valutazioni/Main.java
+++ src/jana60/valutazioni/Main.java
@@ -2,34 +2,52 @@ package jana60.valutazioni;
 
 import java.util.Scanner;
 
-import jana60.shop.Product;
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner scan = new Scanner(System.in);
-		
-		
+
 		System.out.println("Ciao, benvenuto nel nuovo programma di valutazioni!");
-		
+
 		System.out.print("Inserisci quanti Alunni vuoi inserire: ");
-		int sceltautente = scan.nextInt();
-	
-		Studente[] listastudenti = new Studente[sceltautente];
+		int sceltaUtente = scan.nextInt();
 
-		
-		for (int i = 0; i < listastudenti.length; i++) {
-			System.out.println("Inserisci marca: ");
-			String marca = scan.nextLine();
-			System.out.println("Inserisci nome: ");
-			String nome = scan.nextLine();
-			System.out.println("Inserisci prezzo: ");
-			// Aggiunto il parse per evitare errori con lo scan int-double ecc.
-			float prezzo = Float.parseFloat(scan.nextLine());
-			System.out.println("Inserisci Iva: ");
-			int iva = Integer.parseInt(scan.nextLine());
+		Studente[] listaStudenti = new Studente[sceltaUtente];
+
+		for (int i = 0; i < listaStudenti.length; i++) {
+			System.out.println("Inserisci ID dello studente in ordine crescente: ");
+			int id = scan.nextInt();
+
+			System.out.println("Inserisci percentuale di assenze senza il simbolo percentuale: ");
+			int percassenze = scan.nextInt();
+
+			System.out.println("Inserisci la media dei voti dello studente: ");
+			int medVoti = scan.nextInt();
+
+			listaStudenti[i] = new Studente(id, percassenze, medVoti);
+		}
+
+		System.out.println("Ecco la lista degli alunni inseriti con esito della valutazione");
+
+		int totalePromossi = 0;
+		for (int i = 0; i < listaStudenti.length; i++) {
+			System.out.println("Id studente: " + listaStudenti[i].id);
+			System.out.println("Percentuale assenze studente: " + listaStudenti[i].percAssenze + "%");
+			System.out.println("Media voti studente: " + listaStudenti[i].medVoti);
+
+			if (listaStudenti[i].promozioneStudente()) {
+				System.out.println("L'alunno risulta Promosso!");
+				totalePromossi++;
+			} else {
+				System.out.println("L'alunno risulta Bocciato!");
+
+			}
+		}
+
+		System.out.println("Numero di promossi totale degli alunni inseriti è di : " + totalePromossi);
+
+		scan.close();
 	}
-
 }
--- src/jana60/valutazioni/Studente.java
+++ src/jana60/valutazioni/Studente.java
@@ -14,15 +14,18 @@ public class Studente {
 		this.percAssenze = percAssenze;
 		this.medVoti = medVoti;
 
-		if (percAssenze > 25 && percAssenze < 50 && medVoti > 2) {
-			System.out.println("L'alunno selezionato risulta Promosso");
-		} else if (percAssenze < 25 && medVoti >= 2) {
-			System.out.println("L'alunno selezionato risulta Promosso");
-		} else {
-			System.out.println("L'alunno selezionato risulta Bocciato");
-		}
-
-		// Metodi
-
 	}
+
+	// Metodi
+	boolean promozioneStudente() {
+		boolean promozione;
+		if (percAssenze > 25 && percAssenze < 50 && medVoti > 2) {
+			return promozione = true;
+		} else if (percAssenze < 25 && medVoti >= 2) {
+			return promozione = true;
+		} else {
+			return promozione = false;
+		}
+	}
+
 }
